8417024d5568e89621c7b55f660d81f752742578
alagappanpl <31503558+alagappanpl@users.noreply.github.com>
2018-03-02 16:34:19 +0530

    Create 7.8.c

---
+++ 7.8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum*2;
+    
+    if(sum>n)
+    {
+        printf("%d",sum);
+        break;
+    }
+    }
+    return 0;
+}
